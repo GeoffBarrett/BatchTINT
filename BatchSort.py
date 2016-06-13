2bb666768a15455f525b7643589376512448360a
GeoffBarrett <Geoffrey.M.Barrett@gmail.com>
2016-06-13 13:57:16 -0400

    added timestamp functionality

--- BatchSort.py
+++ BatchSort.py
@@ -172,7 +172,7 @@ class Window(QtGui.QWidget): #defines the window class (main window)
                     RunKlustaV2.runKlusta.klusta(self, expt, directory) #runs the function that will perform the klusta'ing
                 except NotADirectoryError:
                     cur_time = datetime.datetime.now().time()
-                    print(str(cur_time)[:8] + ": " + directory + ' is not a directory, skipping!') # if the file is not a directory it prints this message
+                    print(str(cur_time)[:8] + ": " + expt + ' is not a directory, skipping!') # if the file is not a directory it prints this message
                     continue
 
             # --------------------------- makes a while loop that will check for new files to analyze -------------------
