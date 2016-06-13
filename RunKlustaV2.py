fd283aa407f871d37d87c9d8efc10d6ad733a300
GeoffBarrett <Geoffrey.M.Barrett@gmail.com>
2016-06-13 11:47:08 -0400

    working on functionality to ensure that the correct tetrodes are processed

--- RunKlustaV2.py
+++ RunKlustaV2.py
@@ -131,8 +131,7 @@ class runKlusta():
                 # batch = []
                 batch = bytes(
                     'tint ' + '"' + set_path + '" ' + str(tetrode) + ' "' + log_fname + '" /runKK /KKoptions "' +
-                    ini_fname + '"\n'
-                                'exit\n', 'ascii')
+                    ini_fname + '"\n', 'ascii')
 
                 cmd.stdin.write(batch)
                 cmd.stdin.flush()
